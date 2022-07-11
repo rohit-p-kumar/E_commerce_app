5ea5ede7b52fc47c92035b65021ab739806443dc
rohit-p-kumar <pkumar.rohit12@gmail.com>
2022-07-11 20:43:17 +0530

    Started

---
+++ backend/models/productModel.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+
+const productSchema = New.mongoose.Schema({
+    name:{
+        type: String,
+        required: [true,"Please Enter Product Name"],
+        trim: true
+    },
+    description:{
+        type: String,
+        required: [true,"Please Enter Product Description"]
+    },
+    price:{
+        type: Number,
+        required: [true,"Please Enter Product Price"],
+        maxLength: [8,"Price Cannot Exceed 8 Characters"]
+    },
+    rating:{
+        type: Number,
+        default: 0
+    },
+    images:[
+        {public_id:{
+            type: String,
+            required: true
+        },
+        url:{
+            type: String,
+            required: true
+        }}
+    ],
+    category:{
+        type: String,
+        required: [true,"Please Enter Product Category"]
+    },
+    Stock:{
+        type: Number,
+        required: [true,"Please Enter Product Stock"],
+        maxLength: [4,"Stock Cannot Exceed 4 Characters"],
+        default: 1
+    },
+    numofReviews:{
+        type: Number,
+        default: 0
+    },
+    reviews:[
+        {
+            name:{
+                type:String,
+                required:true
+            },
+            rating:{
+                type:Number,
+                required:true
+            },
+            comment:{
+                type:String,
+                required:true
+            },
+        }
+    ],
+    createdAt:{
+        type:Date,
+        default:Date.now
+    }
+
+});
---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const app = require("./app");
+
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
+
+// Config
+
+dotenv.config({path:"backend/config/config.env"});
+
+// Connecting to database
+
+connectDatabase();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server Is Up And Running On Port ${process.env.PORT}`)
+});
